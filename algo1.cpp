cbbf2f5a8537ffb4c2f1ef33e51cd70be594b0b8
Michael Yu <72047491+Michaelyu826@users.noreply.github.com>
2023-02-24 02:37:06 +0000

    MIchael Yu

--- algo1.cpp
+++ algo1.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <climits> // for INT_MAX
+using namespace std;
 
+int find_starting_city(vector<int>& city_distances, vector<int>& fuel, int mpg) {
+    
+    int n = city_distances.size(); // number of cities
+
+    for (int start = 0; start < n; start++) {
+        
+        int tank = 0; // gas in tank
+        
+        int valid = 1; // flag indicating if current starting city is valid
+        
+        
+
+        for (int i = 0; i < n; i++) {
+            
+            int j = (start + i) % n; // index of current city
+            
+            int gas = fuel[j]; // gas available at current city
+            
+            int distance = city_distances[j]; // distance to next city
+            
+            tank += gas * mpg - distance; // update gas in tank after traveling to next city
+
+
+
+            if (tank < 0) {
+                
+                valid = 0; // current starting city is not valid
+                
+                break;
+            }
+        }
+
+        if (valid) {
+            
+            return start;
+            
+        }
+    }
+
+    return -1; // no valid starting city found
+}
+
+int main() {
+    vector<int> city_distances = {5, 20, 15, 10, 15};
+    vector<int> fuel = {1, 2, 1, 0, 3};
+    int mpg = 10;
+    int starting_city = find_starting_city(city_distances, fuel, mpg);
+
+    if (starting_city >= 0) {
+        cout << "Preferred starting city is " << starting_city << endl;
+    } else {
+        cout << "No valid starting city found" << endl;
+    }
+
+    return 0;
+}
